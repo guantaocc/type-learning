1b65c7ee668f049315baad3c9e193e9174b4fe81
guantaocc <1368004939@qq.com>
2022-12-25 21:25:45 +0800

    easy-pick

---
+++ easy-pick/template.ts
@@ -0,0 +1,18 @@
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+// 1. in 遍历联合索引并 
+// 2. keyof 限制 k 为 T类型的索引
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+}
